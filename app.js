30edfb12a62598198f63e634079f3d3ae6e6b645
Asm Shad (INT) <asm.shad@liz.com>
2024-09-25 14:36:15 +0600

    JS Portion Completed

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ function addTask(){
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+
     } 
     inputBox.value = '';  
     saveTask();
@@ -17,6 +22,9 @@ function addTask(){
 listContainer.addEventListener('click', function(e){
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle('checked');
+        saveTask();
+    } else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
         saveTask();
     }
 });
